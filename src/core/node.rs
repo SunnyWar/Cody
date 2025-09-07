bdf4adf17de9e75f5a258242597f1ade8d44e6cd
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-07 13:51:36 -0700

    eliminate lint warnings

--- src/core/node.rs
+++ src/core/node.rs
@@ -1,18 +1,11 @@
 use crate::core::position::Position;
 
 #[derive(Clone)]
+#[derive(Default)]
 pub struct Node {
     pub position: Position,
     pub score: i32,
     pub children: Vec<usize>, // indices into Arena
 }
 
-impl Default for Node {
-    fn default() -> Self {
-        Self {
-            position: Position::default(),
-            score: 0,
-            children: Vec::new(),
-        }
-    }
-}
+
