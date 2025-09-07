fa2da8f0cf80ca4378a0793ab8907e339a1316d3
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-07 13:51:36 -0700

    eliminate lint warnings

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use Cody::core::position::Position;
-use Cody::search::{engine::Engine, evaluator::MaterialEvaluator, movegen::SimpleMoveGen};
+#![allow(non_snake_case)]
+
+use cody::core::position::Position;
+use cody::search::{engine::Engine, evaluator::MaterialEvaluator, movegen::SimpleMoveGen};
 
 fn main() {
     let mut engine = Engine::new(1024, SimpleMoveGen, MaterialEvaluator);
