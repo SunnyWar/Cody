646dbe146064f259a85195557e7f7f82dcddca97
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-07 17:36:44 -0700

    check if king in in check

--- src/lib.rs
+++ src/lib.rs
@@ -3,5 +3,5 @@ pub mod search;
 
 pub use crate::core::position::Position;
 pub use crate::search::engine::{Engine, NODE_COUNT};
-pub use crate::search::movegen::SimpleMoveGen;
 pub use crate::search::evaluator::MaterialEvaluator;
+pub use crate::search::movegen::SimpleMoveGen;
