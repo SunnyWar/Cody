de2feebd28755102652c75e821cb277447c9e291
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-08 12:09:57 -0700

    Use enum for color instead of magic numbers

--- src/core/piece.rs
+++ src/core/piece.rs
@@ -8,6 +8,27 @@ pub enum PieceType {
     King,
 }
 
+#[derive(Clone, Copy, PartialEq)]
+pub enum Color {
+    White = 0,
+    Black = 1,
+}
+
+impl Color {
+    #[inline]
+    pub fn opposite(self) -> Color {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
+}
+
+pub struct Piece {
+    pub color: Color,
+    pub kind: PieceType,
+}
+
 impl PieceType {
     /// Parse from a promotion character in UCI notation ('q','r','b','n')
     pub fn from_uci(ch: char) -> Option<Self> {
@@ -18,5 +39,23 @@ impl PieceType {
             'n' => Some(PieceType::Knight),
             _ => None,
         }
+    }
+}
+
+#[inline]
+pub const fn piece_index(color: Color, kind: PieceType) -> usize {
+    match (color, kind) {
+        (Color::White, PieceType::Pawn) => 0,
+        (Color::White, PieceType::Knight) => 1,
+        (Color::White, PieceType::Bishop) => 2,
+        (Color::White, PieceType::Rook) => 3,
+        (Color::White, PieceType::Queen) => 4,
+        (Color::White, PieceType::King) => 5,
+        (Color::Black, PieceType::Pawn) => 6,
+        (Color::Black, PieceType::Knight) => 7,
+        (Color::Black, PieceType::Bishop) => 8,
+        (Color::Black, PieceType::Rook) => 9,
+        (Color::Black, PieceType::Queen) => 10,
+        (Color::Black, PieceType::King) => 11,
     }
 }
