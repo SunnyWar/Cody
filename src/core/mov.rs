b4731da1c7586b0937c2f063290964a0c05c6042
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-07 17:54:40 -0700

    end of search returns best move

--- src/core/mov.rs
+++ src/core/mov.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 #[derive(Clone, Copy, Debug, PartialEq, Eq)]
 pub struct Move {
     pub from: u8,      // 0..63
@@ -14,5 +16,38 @@ impl Move {
             promotion: 255, // 255 = none
             flags: 0,       // no special flags
         }
+    }
+
+    pub fn null() -> Self {
+        Move {
+            from: 0,
+            to: 0,
+            promotion: 255, // none
+            flags: 0,
+        }
+    }
+}
+
+impl Move {
+    pub fn from_square(&self) -> String {
+        square_to_string(self.from)
+    }
+
+    pub fn to_square(&self) -> String {
+        square_to_string(self.to)
+    }
+}
+
+fn square_to_string(sq: u8) -> String {
+    let file = sq % 8;
+    let rank = sq / 8;
+    let file_char = (b'a' + file) as char;
+    let rank_char = (b'1' + rank) as char;
+    format!("{}{}", file_char, rank_char)
+}
+
+impl fmt::Display for Move {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}{}", self.from_square(), self.to_square())
     }
 }
