18ef1afb49010d49369f8583ae9339eb95697596
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-19 20:44:33 -0700

    move constants to constants.rs

--- bitboard/src/constants.rs
+++ bitboard/src/constants.rs
@@ -1,3 +1,12 @@
 // bitboard/src/constants.rs
 pub const BOARD_SIZE: usize = 8;
 pub const NUM_SQUARES: usize = BOARD_SIZE * BOARD_SIZE;
+
+pub const NORTH: i8 = 8;
+pub const SOUTH: i8 = -8;
+pub const NORTH_EAST: i8 = 9;
+pub const NORTH_WEST: i8 = 7;
+pub const SOUTH_EAST: i8 = -7;
+pub const SOUTH_WEST: i8 = -9;
+pub const DOUBLE_NORTH: i8 = 16;
+pub const DOUBLE_SOUTH: i8 = -16;
