aa82e79aa3fa2e4f5bca4d32d7e5a2b121f9d3f2
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-09-21 13:45:04 -0700

    king extraction

--- bitboard/src/movegen/api.rs
+++ bitboard/src/movegen/api.rs
@@ -329,32 +329,7 @@ fn generate_pseudo_king_moves(
     context: &MoveGenContext,
     moves: &mut Vec<ChessMove>,
 ) {
-    let king_bb = pos
-        .pieces
-        .get(Piece::from_parts(context.us, Some(PieceKind::King)));
-
-    if let Some(from) = king_bb.squares().next() {
-        // Standard king moves
-        let valid_moves = king_attacks(from).and(context.not_ours);
-        push_moves_from_valid_targets(pos, context, from, valid_moves, moves);
-
-        // Castling moves
-        if pos.can_castle_kingside(context.us) {
-            let to = match context.us {
-                Color::White => Square::G1,
-                Color::Black => Square::G8,
-            };
-            moves.push(ChessMove::new(from, to, MoveType::CastleKingside));
-        }
-
-        if pos.can_castle_queenside(context.us) {
-            let to = match context.us {
-                Color::White => Square::C1,
-                Color::Black => Square::C8,
-            };
-            moves.push(ChessMove::new(from, to, MoveType::CastleQueenside));
-        }
-    }
+    crate::movegen::generate_pseudo_king_moves(pos, context, moves);
 }
 
 #[cfg(test)]
