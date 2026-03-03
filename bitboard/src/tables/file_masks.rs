034f5afe38f5d76ac4ad0c1390a42f45d9fa41e5
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2026-03-03 14:45:38 -0700

    format numbers

--- bitboard/src/tables/file_masks.rs
+++ bitboard/src/tables/file_masks.rs
@@ -2,21 +2,22 @@
 use crate::BitBoardMask;
 use crate::constants::BOARD_SIZE;
 
-pub const FILE_A: BitBoardMask = BitBoardMask(0x0101010101010101);
-pub const FILE_B: BitBoardMask = BitBoardMask(0x0202020202020202);
-pub const FILE_C: BitBoardMask = BitBoardMask(0x0404040404040404);
-pub const FILE_D: BitBoardMask = BitBoardMask(0x0808080808080808);
-pub const FILE_E: BitBoardMask = BitBoardMask(0x1010101010101010);
-pub const FILE_F: BitBoardMask = BitBoardMask(0x2020202020202020);
-pub const FILE_G: BitBoardMask = BitBoardMask(0x4040404040404040);
-pub const FILE_H: BitBoardMask = BitBoardMask(0x8080808080808080);
+pub const FILE_A: BitBoardMask = BitBoardMask(0x0101_0101_0101_0101);
+pub const FILE_B: BitBoardMask = BitBoardMask(0x0202_0202_0202_0202);
+pub const FILE_C: BitBoardMask = BitBoardMask(0x0404_0404_0404_0404);
+pub const FILE_D: BitBoardMask = BitBoardMask(0x0808_0808_0808_0808);
+pub const FILE_E: BitBoardMask = BitBoardMask(0x1010_1010_1010_1010);
+pub const FILE_F: BitBoardMask = BitBoardMask(0x2020_2020_2020_2020);
+pub const FILE_G: BitBoardMask = BitBoardMask(0x4040_4040_4040_4040);
+pub const FILE_H: BitBoardMask = BitBoardMask(0x8080_8080_8080_8080);
 
 pub const FILE_MASKS: [BitBoardMask; BOARD_SIZE] = [
     FILE_A, FILE_B, FILE_C, FILE_D, FILE_E, FILE_F, FILE_G, FILE_H,
 ];
 
 // Masks excluding specific files
-pub const NOT_FILE_A: BitBoardMask = BitBoardMask(0xFEFEFEFEFEFEFEFE);
-pub const NOT_FILE_AB: BitBoardMask = BitBoardMask(0xFCFCFCFCFCFCFCFC);
-pub const NOT_FILE_H: BitBoardMask = BitBoardMask(0x7F7F7F7F7F7F7F7F);
-pub const NOT_FILE_GH: BitBoardMask = BitBoardMask(0x3F3F3F3F3F3F3F3F);
+pub const NOT_FILE_A: BitBoardMask = BitBoardMask(0xFEFE_FEFE_FEFE_FEFE);
+pub const NOT_FILE_AB: BitBoardMask = BitBoardMask(0xFCFC_FCFC_FCFC_FCFC);
+pub const NOT_FILE_H: BitBoardMask = BitBoardMask(0x7F7F_7F7F_7F7F_7F7F);
+pub const NOT_FILE_GH: BitBoardMask = BitBoardMask(0x3F3F_3F3F_3F3F_3F3F);
+
